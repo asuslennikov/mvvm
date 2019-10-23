c9b13ecc835121d4affbcbb9a199502b9e3635b9
Anton Suslennikov <Anton.Suslennikov@bsc-ideas.com>
2019-10-23 22:54:21 +0300

    Translate javadoc in mvvm-api module, package domain

--- mvvm-api/src/main/java/com/github/asuslennikov/mvvm/api/domain/UseCase.java
+++ mvvm-api/src/main/java/com/github/asuslennikov/mvvm/api/domain/UseCase.java
@@ -1,47 +1,55 @@
 /**
  * Copyright 2019 Suslennikov Anton
  * <p>
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
  * <p>
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package com.github.asuslennikov.mvvm.api.domain;
 
 
 import androidx.annotation.NonNull;
 
+import io.reactivex.BackpressureStrategy;
+import io.reactivex.Flowable;
 import io.reactivex.Observable;
+import io.reactivex.Scheduler;
+import io.reactivex.functions.Consumer;
 
 /**
- * Контракт сценария бизнес-логики.
+ * It is general contract which allow any caller to execute a specific business scenario with
+ * given input and receive an output. By term 'business scenario' we assume some set of actions
+ * (or predefined rules, or data transformation steps) which are performed on given input. When
+ * use case completes (or fails to complete) these actions, it can provide some information in
+ * response or just notify about the end of work.
  * <p></p>
- * <b>Зона ответственности:</b> Содержит бизнес-логику приложения (обработка данных по определенным правилам).
- * <p></p>
- * Предполагается, что реализации содержат в себе бизнес-правила и логику работы приложения,
- * взаимодействуют с data-слоем приложения (менеджерами).
+ * Example: CreateRepositoryUseCase - it takes some info (repository name, maybe access rights),
+ * performs necessary actions (connects to the server, checks correctness, executes creation steps)
+ * and as a result returns repository address in case of success, or reason of fail.
  *
- * @param <IN>  Представление входных данных
- * @param <OUT> Представление результата работы
+ * @param <IN>  use case input information
+ * @param <OUT> result of use case's work
  */
 public interface UseCase<IN extends UseCaseInput, OUT extends UseCaseOutput> {
     /**
-     * Запускает бизнес-сценарий (выполняет какую-либо команду). Во время работы данного метода
-     * изменяются данные, выполняются запросы и т.д. Поток выполнения определяется самим сценарием.
-     * Может занимать продолжительное время (зависит от реализации).
+     * This method triggers use case execution. An actual work thread should be chosen by
+     * implementation itself (implementation should call {@link Observable#subscribeOn(Scheduler)}).
+     * Use case implementation shouldn't care about back-pressure, it's  caller's responsibility
+     * to convert {@link Observable} to {@link Flowable} (for example with
+     * ({@link Observable#toFlowable(BackpressureStrategy)}).
      * <p></p>
-     * В большинстве реализаций, выполнение сценария начнется только после регистрации слушателя
-     * для результата работы.
+     * In most implementations an actual work will be started only when caller subscribes to
+     * observable (return value), see {@link Observable#subscribe(Consumer, Consumer)}
      *
-     * @param useCaseInput входные данные, используемые внутри сценария
-     * @return объект для регистрации получателя результата работы (а также промежуточных данных)
+     * @param useCaseInput input information for use case
+     * @return object for registering a subscriber for work's result (and also intermediate
+     * notifications, if the implementation provides any)
      */
     Observable<OUT> execute(@NonNull IN useCaseInput);
 }
