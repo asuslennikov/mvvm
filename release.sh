28a04644b40824442beaa61bacd1581cf16ba8c5
Anton Suslennikov <Anton.Suslennikov@bsc-ideas.com>
2019-10-31 21:34:30 +0300

    Release repository in Maven Central

--- release.sh
+++ release.sh
@@ -6,4 +6,4 @@ openssl aes-256-cbc -k "$SIGN_KEY" -in release.enc -out release.key -d &&
 # Publish to Bintray
 ./gradlew publishAllPublicationsToBintrayRepository &&
 # Publish to MavenCentral
-./gradlew :mvvm-api:initializeSonatypeStagingRepository :mvvm-domain:initializeSonatypeStagingRepository :mvvm-presentation:initializeSonatypeStagingRepository publishToSonatype closeRepository
+./gradlew :mvvm-api:initializeSonatypeStagingRepository :mvvm-domain:initializeSonatypeStagingRepository :mvvm-presentation:initializeSonatypeStagingRepository publishToSonatype closeAndReleaseRepository
