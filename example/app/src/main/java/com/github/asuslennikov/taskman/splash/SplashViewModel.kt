bcef7d7ce3d2b32704fae92958b0ecab63bdb3ae
Anton Suslennikov <Anton.Suslennikov@bsc-ideas.com>
2020-01-09 19:05:54 +0300

    Finish animation when app is loaded

--- example/app/src/main/java/com/github/asuslennikov/taskman/splash/SplashViewModel.kt
+++ example/app/src/main/java/com/github/asuslennikov/taskman/splash/SplashViewModel.kt
@@ -1,9 +1,39 @@
 package com.github.asuslennikov.taskman.splash
 
 import com.github.asuslennikov.mvvm.presentation.AbstractViewModel
+import com.github.asuslennikov.taskman.R
 import com.github.asuslennikov.taskman.TaskApplication
+import io.reactivex.Observable
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.functions.BiFunction
+import java.util.concurrent.TimeUnit
 
 class SplashViewModel(private val app: TaskApplication) : AbstractViewModel<SplashState>() {
+    private val disposable = CompositeDisposable()
+
+    init {
+        disposable.add(Observable.combineLatest(
+            applicationLoading(),
+            minimalLoading(),
+            BiFunction { l1: TaskApplication.InitializationStatus, _: TaskApplication.InitializationStatus -> l1 }
+        ).subscribe { onLoadingCompleted() })
+    }
+
+    private fun minimalLoading(): Observable<TaskApplication.InitializationStatus> {
+        return Observable.timer(app.resources.getInteger(R.integer.splash_visibility_minimum_duration).toLong(), TimeUnit.MILLISECONDS)
+            .map { TaskApplication.InitializationStatus.COMPLETE }
+    }
+
+    private fun applicationLoading(): Observable<TaskApplication.InitializationStatus> = app.getInitializationStatus()
+
+    private fun onLoadingCompleted() {
+        sendState(SplashState(false))
+    }
+
     override fun buildInitialState(): SplashState = SplashState(true)
 
+    override fun onCleared() {
+        super.onCleared()
+        disposable.clear()
+    }
 }
